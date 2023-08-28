7d66eadcd6bc8b7c3d7a28d83e5bf433485c21d1
TaixMiguel <TaixMiguel@desktopLenovo-Ubuntu>
2023-08-28 19:52:21 +0200

    [FEATURE] Generación del proyecto vacío

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taix Spring Backups
+
+Proyecto para generar backups y almacenarlos en Mega generado utilizando el framework Spring Boot
---
+++ SpringBackups/src/main/java/es/taixmiguel/springbackups/SpringBackupsApplication.java
@@ -0,0 +1,13 @@
+package es.taixmiguel.springbackups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackupsApplication.class, args);
+	}
+
+}
